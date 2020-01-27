7853edbc364f0e00af57396204dbef2d843b8113
maxkorzan <maxkorzan@gmail.com>
2020-01-27 17:06:21 -0500

    corrected rows/columns output

--- src/Main.java
+++ src/Main.java
@@ -33,8 +33,8 @@ public class Main {
 
         int[][] rowsAndColumns = new int[5][10];
 
-        for(int i=0; i<rowsAndColumns.length; i++){
-            for(int j=0; j<rowsAndColumns.length; j++){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<5; j++){
                 System.out.print("row "+i+" col "+j+" ");
             }
             System.out.println();
