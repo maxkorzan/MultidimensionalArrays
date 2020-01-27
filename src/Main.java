6df6531314eb15a6efef2fde2ff51835324570e7
maxkorzan <maxkorzan@gmail.com>
2020-01-27 17:14:18 -0500

    removed unnecessary code/comments

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,7 @@
 public class Main {
-    //print rows (1-10)
-    //print columns (1-5)
-
     public static void main(String[] args) {
-//
-//        // array of gender, major, state, score
-//        String[][] studentGradebook = new String[100][4];
-//
-//        studentGradebook[0][0] = "M";
-//        studentGradebook[0][1] = "Econ";
-//        studentGradebook[0][2] = "MD";
-//        studentGradebook[0][3] = "99";
-//
-//        studentGradebook[1][0] = "F";
-//        studentGradebook[1][1] = "Mktg";
-//        studentGradebook[1][2] = "VA";
-//        studentGradebook[1][3] = "89";
-//
-//        studentGradebook[2][0] = "F";
-//        studentGradebook[2][1] = "Biol";
-//        studentGradebook[2][2] = "NJ";
-//        studentGradebook[2][3] = "100";
-//
-//        for (int i = 0; i < 3; i++) {
-//            System.out.print("Student "+i+'\t');
-//            for (int j = 0; j < 4; j++) {
-//                System.out.print(studentGradebook[i][j] + '\t');
-//            }
-//            System.out.println();
-//        }
-//    }
 
+        //PRINT ROWS AND COLUMNS
         int[][] rowsAndColumns = new int[5][10];
 
         for(int i=0; i<10; i++){
@@ -51,12 +22,6 @@ public class Main {
             }
             System.out.println();
         }
-
-
-
-
-
-
 
     }
 
